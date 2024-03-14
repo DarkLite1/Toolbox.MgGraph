8c5fe4ed40c21c3424eb0c7496573b1902848399
DarkLite1 <BrechtGijbels@gmail.com>
2024-03-14 11:18:18 +0100

    module born

---
+++ Toolbox.MgGraph.Tests.ps1
@@ -0,0 +1,54 @@
+﻿#Requires -Version 7
+#Requires -Modules Pester
+
+BeforeDiscovery {
+    # used by inModuleScope
+    $testModule = $PSCommandPath.Replace('.Tests.ps1', '.psm1')
+    $testModuleName = $testModule.Split('\')[-1].TrimEnd('.psm1')
+
+    Remove-Module $testModuleName -Force -Verbose:$false -EA Ignore
+    Import-Module $testModule -Force -Verbose:$false
+}
+Describe 'ConvertTo-MgUserMailRecipientHC' {
+    Context 'create a hashtable for one e-mail address' {
+        BeforeAll {
+            $params = @{
+                MailAddress = 'bob@conotoso.com'
+            }
+            $actual = ConvertTo-MgUserMailRecipientHC @params
+        }
+        It 'with EmailAddress.Address' {
+            $actual.EmailAddress.Address | Should -BeExactly $params.MailAddress
+        }
+        It 'with the correct quantity of hashtables' {
+            $actual.Count | Should -Be $params.MailAddress.Count
+        }
+        It 'of type array' {
+            $actual.getType().BaseType.Name | Should -BeExactly 'Array'
+        }
+    }
+    Context 'create a hashtable for multiple e-mail addresses' {
+        BeforeAll {
+            $params = @{
+                MailAddress = 'bob@conotoso.com', 'mike@conotoso.com'
+            }
+            $actual = ConvertTo-MgUserMailRecipientHC @params
+        }
+        It 'with EmailAddress.Address' {
+            $params.MailAddress.foreach(
+                {
+                    $actual.EmailAddress.Values |
+                    Should -Contain $_
+                }
+            )
+            $actual.EmailAddress.Keys.ForEach({$_ |
+                Should -BeExactly 'Address'})
+        }
+        It 'with the correct quantity of hashtables' {
+            $actual.Count | Should -Be $params.MailAddress.Count
+        }
+        It 'of type array' {
+            $actual.getType().BaseType.Name | Should -BeExactly 'Array'
+        }
+    }
+}
---
+++ Toolbox.MgGraph.psm1
@@ -0,0 +1,48 @@
+﻿#Requires -Version 7
+#Requires -Modules Microsoft.Graph.Authentication
+
+try {
+    $params = @{
+        ClientId              = $env:AZURE_CLIENT_ID
+        TenantId              = $env:AZURE_TENANT_ID
+        CertificateThumbprint = $env:AZURE_POWERSHELL_CERTIFICATE_THUMBPRINT
+        NoWelcome             = $true
+        ErrorAction           = 'Stop'
+    }
+    Connect-MgGraph @params
+}
+catch {
+    throw "Failed authenticating to MS Graph: $_"
+}
+
+Function ConvertTo-MgUserMailRecipientHC {
+    <#
+        .SYNOPSIS
+            Helper function for Send-MgUserMail to create a list of
+            e-mail addresses that can be used with ToRecipients,
+            CcRecipients and BccRecipients.
+
+        .EXAMPLE
+            $params = @{
+                MailAddress = 'bob@conotoso.com', 'mike@conotoso.com'
+            }
+            ConvertTo-MgUserMailRecipientHC @params
+    #>
+
+    [CmdLetBinding()]
+    [OutputType([hashtable[]])]
+    Param(
+        [Parameter(Mandatory)]
+        [String[]]$MailAddress
+    )
+
+    $result = @()
+
+    $MailAddress.ForEach(
+        {
+            $result += @{EmailAddress = @{Address = $_ } }
+        }
+    )
+
+    , $result
+}
